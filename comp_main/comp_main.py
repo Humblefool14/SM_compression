ad9fe4201fd3e67dca838018e4a40dcda074c9b1
Humblefool14 <srikarmaturi97@gmail.com>
2021-11-22 21:04:02 +0530

    initial runner_template

---
+++ comp_main/comp_main.py
@@ -0,0 +1,19 @@
+def func(pkg): 
+    if(pkg==0):
+        print("No package")
+        return 0
+    elif:
+        print("Package Initiated")
+        return(pkg%100)
+
+def comp(x):
+    compare_1_coeff = wavelet(x)
+    compare_2_coeff = vector(x)
+    z compare(compare_1_coeff,compare_2_coeff)
+    return comp
+
+def int_massive(pkg):
+    x = func(pkg)
+    y = comp(x)
+    z = perf(y)
+
