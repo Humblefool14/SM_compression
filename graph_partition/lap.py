aa2f955230f12996a5e6f625e83dc7360f52a714
Humblefool14 <srikarmaturi97@gmail.com>
2021-11-24 00:36:41 +0530

    smoothness_func added

--- graph_partition/lap.py
+++ graph_partition/lap.py
@@ -15,3 +15,22 @@ def L_normalized(u,v):
         return 1/math.sqrt(diag(u)*diag(v))
     else return 0
 
+def fnc(i):
+    return i*i+2*i+1
+    ##implement function
+
+def sq(i):
+    return i*i
+    
+def grad_fn(i,j):
+    return weight(i,j)*(fnc(i)-fnc(j))
+
+def smoothness_grph(graph_struct):
+    path_sum = []
+    for count,v in enumerate(graph_struct):
+        path_sum += sq(fnc(v))
+        for u in graph_struct:
+            if(u!=v):
+                path_sum+=wt(u,v)*sq(abs(fnc(u)-fnc(v)))
+
+
